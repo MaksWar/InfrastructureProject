752700c07ba71d523c68c172593b5c220421ef16
MaksWar <maksimkiiko2001@gmail.com>
2022-04-27 23:36:09 +0300

    - Task-9
    > Добавлена логика поворота мельницы
    > Декорация деревни

---
+++ Assets/GameFiles/Scripts/BuildsLogic/ObjectRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace GameFiles.Scripts.BuildsLogic
+{
+	public class ObjectRotator : MonoBehaviour
+	{
+		[SerializeField] private Transform target;
+
+		[SerializeField] private float speed;
+
+		private void Update()
+		{
+			target.Rotate(new Vector3(0, 0, speed * Time.deltaTime), Space.Self);
+		}
+	}
+}
